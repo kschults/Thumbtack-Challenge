f389cab5c6cc58c2aded097a42dbd7c464d0e1b6
Karl Schults <karl@secondglass.com>
2012-09-30 10:57:26 -0700

    Create XCode project

---
+++ Thumbtack Challenge/Thumbtack Challenge/KTCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KTCAppDelegate.h
+//  Thumbtack Challenge
+//
+//  Created by Karl Schults on 9/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KTCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Thumbtack Challenge/Thumbtack Challenge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thumbtack Challenge
+//
+//  Created by Karl Schults on 9/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KTCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KTCAppDelegate class]));
+    }
+}
